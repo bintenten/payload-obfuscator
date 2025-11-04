a60d598b4a090fe0e578232fb537d773fef0d028
veryglue@gmail.com <veryglue@gmail.com>
2025-11-04 15:01:43 -0500

    Outputing decimal representation

--- obfuscator.py
+++ obfuscator.py
@@ -1,16 +1,25 @@
 import sys
 
 def read_file(filename):
+	text = None
 	try:
 		with open(filename, "r") as f:
-			text = f.read()
-		return text
+			text = f.read().strip()
 	except FileNotFoundError:
 		print(f"Error: the file {filename} does not exist.")
+	finally:
+		return text
 
 def obfuscate(filename):
 	text = read_file(filename)
+	if not text:
+		print(f"File {filename} is empty")
+		exit(1)
 
+	orded = []
+	for char in text:
+		orded.append(ord(char))
+	print(orded)
 
 
 if __name__ == "__main__":
